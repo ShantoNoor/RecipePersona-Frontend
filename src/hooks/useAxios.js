40fc0ada9a2070d60a4e2d5c686f87ad406d6ff1
Shanto Noor <shantonoor900@gmail.com>
2024-04-04 13:17:21 +0600

    backend link added

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
   baseURL: import.meta.env.PROD
-    ? "https://backend-nine-zeta-50.vercel.app"
+    ? "https://recipe-persona-backend.vercel.app"
     : "http://localhost:3000",
 });
 
