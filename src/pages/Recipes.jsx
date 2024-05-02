8944a736cd5d3d52540bab499d86b2dbeca44b2c
Shanto Noor <shantonoor900@gmail.com>
2024-05-02 15:42:57 +0600

    Pagination added

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -21,10 +21,21 @@ import { useState } from "react";
 
 const MotionCard = motion(Card);
 
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+} from "@/components/ui/pagination";
+
 const Recipes = () => {
   const navigate = useNavigate();
 
   const [filterData, setFilterData] = useState([]);
+
+  const itemsPerPage = 8;
+  const totalPages = Math.ceil(filterData.length / itemsPerPage);
+  const [currentPage, setCurrentPage] = useState(7);
 
   const { data, error, isPending } = useQuery({
     queryKey: ["recipes"],
@@ -49,75 +60,95 @@ const Recipes = () => {
         <Input
           type="search"
           placeholder="Search recipe here ... "
-          onChange={(e) => setFilterData(
-            data.filter(
-              (recipe) =>
+          onChange={(e) => {
+            setFilterData(
+              data.filter((recipe) =>
                 recipe.name.toLowerCase().includes(e.target.value.toLowerCase())
-            )
-          )}
+              )
+            );
+            setCurrentPage(0);
+          }}
         />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        {filterData?.map((recipe, idx) => (
-          <MotionCard
-            key={recipe._id}
-            onClick={() => navigate(`/view-recipe/${recipe._id}`)}
-            className="flex flex-col overflow-hidden rounded-lg shadow-md cursor-pointer"
-            initial={{ opacity: 0, y: 30 }}
-            animate={{
-              opacity: 1,
-              y: 0,
-              transition: {
-                duration: 0.5,
-                delay: 0.15 * idx,
-              },
-            }}
-            whileHover={{
-              translateY: -10,
-            }}
-            whileTap={{
-              translateY: -5,
-            }}
-          >
-            <CardHeader className="flex flex-row gap-4 items-center p-4">
-              <Avatar>
-                <AvatarImage
-                  src={recipe.author.photo}
-                  className="object-cover w-12 h-12 rounded-full shadow"
-                />
-                <AvatarFallback>{recipe.author.name}</AvatarFallback>
-              </Avatar>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
+        {filterData
+          ?.slice(currentPage * itemsPerPage, itemsPerPage * (currentPage + 1))
+          .map((recipe, idx) => (
+            <MotionCard
+              key={recipe._id}
+              onClick={() => navigate(`/view-recipe/${recipe._id}`)}
+              className="flex flex-col overflow-hidden rounded-lg shadow-md cursor-pointer"
+              initial={{ opacity: 0, y: 30 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  duration: 0.5,
+                  delay: 0.15 * idx,
+                },
+              }}
+              whileHover={{
+                translateY: -10,
+              }}
+              whileTap={{
+                translateY: -5,
+              }}
+            >
+              <CardHeader className="flex flex-row gap-4 items-center p-4">
+                <Avatar>
+                  <AvatarImage
+                    src={recipe.author.photo}
+                    className="object-cover w-12 h-12 rounded-full shadow"
+                  />
+                  <AvatarFallback>{recipe.author.name}</AvatarFallback>
+                </Avatar>
 
-              <div className="flex flex-col space-y-1">
-                <span className="text-sm font-semibold">
-                  {recipe.author.name}
-                </span>
-                <span className="text-xs">
-                  {getTimeAgoString(recipe.createdAt)}
-                </span>
+                <div className="flex flex-col space-y-1">
+                  <span className="text-sm font-semibold">
+                    {recipe.author.name}
+                  </span>
+                  <span className="text-xs">
+                    {getTimeAgoString(recipe.createdAt)}
+                  </span>
+                </div>
+              </CardHeader>
+              <div className="px-4">
+                <Separator />
               </div>
-            </CardHeader>
-            <div className="px-4">
-              <Separator />
-            </div>
-            <CardContent className="p-4">
-              <Image
-                src={recipe.image}
-                alt={recipe.name}
-                className="object-cover aspect-square w-full mb-4 rounded-md"
-              />
-              <CardTitle className="mb-1 text-xl font-semibold">
-                {recipe.name}
-              </CardTitle>
-              <CardDescription className="text-sm">
-                {recipe.instructions.length > 100
-                  ? `${recipe.instructions.substring(0, 100)} ...`
-                  : recipe.instructions}
-              </CardDescription>
-            </CardContent>
-          </MotionCard>
-        ))}
+              <CardContent className="p-4">
+                <Image
+                  src={recipe.image}
+                  alt={recipe.name}
+                  className="object-cover aspect-square w-full mb-4 rounded-md"
+                />
+                <CardTitle className="mb-1 text-xl font-semibold">
+                  {recipe.name}
+                </CardTitle>
+                <CardDescription className="text-sm">
+                  {recipe.instructions.length > 100
+                    ? `${recipe.instructions.substring(0, 100)} ...`
+                    : recipe.instructions}
+                </CardDescription>
+              </CardContent>
+            </MotionCard>
+          ))}
       </div>
+
+      <Pagination>
+        <PaginationContent>
+          {[...Array(totalPages).keys()].map((page) => (
+            <PaginationItem
+              className="cursor-pointer"
+              key={page}
+              onClick={() => setCurrentPage(page)}
+            >
+              <PaginationLink isActive={currentPage === page}>
+                {page + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+        </PaginationContent>
+      </Pagination>
     </>
   );
 };
