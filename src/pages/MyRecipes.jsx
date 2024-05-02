8cfd0b3c70587b7314c9bcc0d0ca9e0f0a451f82
Shanto Noor <shantonoor900@gmail.com>
2024-05-02 17:16:30 +0600

    Pagination added to my recipes page

--- src/pages/MyRecipes.jsx
+++ src/pages/MyRecipes.jsx
@@ -28,12 +28,25 @@ import Image from "@/components/Image";
 import { Separator } from "@/components/ui/separator";
 import Title from "@/components/Title";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const MotionCard = motion(Card);
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+} from "@/components/ui/pagination";
 
 const MyRecipes = () => {
   const { user } = useAuth();
   const navigate = useNavigate();
+
+  const [filterData, setFilterData] = useState([]);
+  const itemsPerPage = 8;
+  const totalPages = Math.ceil(filterData.length / itemsPerPage);
+  const [currentPage, setCurrentPage] = useState(0);
 
   const { data, error, isPending, refetch } = useQuery({
     queryKey: ["recipes", `author=${user?._id}`],
@@ -41,6 +54,7 @@ const MyRecipes = () => {
     queryFn: async () => {
       try {
         const result = await axiosPublic.get(`/recipes?author=${user._id}`);
+        setFilterData(result.data);
         return result.data;
       } catch (err) {
         console.error("Error fetching recipes:", err);
@@ -54,112 +68,130 @@ const MyRecipes = () => {
   return (
     <>
       <Title>My Recipes</Title>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        {data.map((recipe, idx) => (
-          <MotionCard
-            key={recipe._id}
-            className="flex flex-col overflow-hidden rounded-lg shadow-md"
-            initial={{ opacity: 0, y: 30 }}
-            animate={{
-              opacity: 1,
-              y: 0,
-              transition: {
-                duration: 0.5,
-                delay: 0.15 * idx,
-              },
-            }}
-            whileHover={{
-              translateY: -10,
-            }}
-            whileTap={{
-              translateY: -5,
-            }}
-          >
-            <div
-              onClick={() => navigate(`/view-recipe/${recipe._id}`)}
-              className="cursor-pointer flex-1"
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
+        {filterData
+          ?.slice(currentPage * itemsPerPage, itemsPerPage * (currentPage + 1))
+          .map((recipe, idx) => (
+            <MotionCard
+              key={recipe._id}
+              className="flex flex-col overflow-hidden rounded-lg shadow-md"
+              initial={{ opacity: 0, y: 30 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  duration: 0.5,
+                  delay: 0.15 * idx,
+                },
+              }}
+              whileHover={{
+                translateY: -10,
+              }}
+              whileTap={{
+                translateY: -5,
+              }}
             >
-              <CardContent className="p-4">
-                <Image
-                  src={recipe.image}
-                  alt={recipe.name}
-                  className="object-cover aspect-square w-full mb-4 rounded-md"
-                />
-                <CardTitle className="mb-1 text-xl font-semibold">
-                  {recipe.name}
-                </CardTitle>
-                <CardDescription className="text-sm">
-                  {recipe.instructions.length > 100
-                    ? `${recipe.instructions.substring(0, 100)} ...`
-                    : recipe.instructions}
-                </CardDescription>
-              </CardContent>
-            </div>
-            <div className="px-4">
-              <Separator />
-            </div>
-            <CardFooter className="flex flex-wrap justify-end p-4">
-              <div className="space-x-2">
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  onClick={() => navigate(`/view-recipe/${recipe._id}`)}
-                >
-                  <EyeIcon className="size-4 text-primary" />
-                </Button>
-
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  onClick={() => navigate(`/update-recipe/${recipe._id}`)}
-                >
-                  <Pencil className="size-4 text-primary" />
-                </Button>
-
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <Button variant="ghost" size="icon">
-                      <Trash className="size-4 text-primary" />
-                    </Button>
-                  </DialogTrigger>
-                  <DialogContent>
-                    <DialogHeader>
-                      <DialogTitle>Are you absolutely sure?</DialogTitle>
-                      <DialogDescription>
-                        This action cannot be undone. This will permanently
-                        delete your recipe and remove the recipe from the
-                        database.
-                      </DialogDescription>
-                    </DialogHeader>
-                    <DialogFooter>
-                      <Button
-                        onClick={() => {
-                          toast.promise(
-                            axiosPublic.delete(`/recipes/${recipe._id}`),
-                            {
-                              loading: "Deleting recipe, Please wait ...",
-                              success: () => {
-                                refetch();
-                                return "Recipe deleted successfully";
-                              },
-                              error: (err) => {
-                                toast.error("Failed to delete recipe");
-                                return err.message;
-                              },
-                            }
-                          );
-                        }}
-                      >
-                        Delete
-                      </Button>
-                    </DialogFooter>
-                  </DialogContent>
-                </Dialog>
+              <div
+                onClick={() => navigate(`/view-recipe/${recipe._id}`)}
+                className="cursor-pointer flex-1"
+              >
+                <CardContent className="p-4">
+                  <Image
+                    src={recipe.image}
+                    alt={recipe.name}
+                    className="object-cover aspect-square w-full mb-4 rounded-md"
+                  />
+                  <CardTitle className="mb-1 text-xl font-semibold">
+                    {recipe.name}
+                  </CardTitle>
+                  <CardDescription className="text-sm">
+                    {recipe.instructions.length > 100
+                      ? `${recipe.instructions.substring(0, 100)} ...`
+                      : recipe.instructions}
+                  </CardDescription>
+                </CardContent>
               </div>
-            </CardFooter>
-          </MotionCard>
-        ))}
+              <div className="px-4">
+                <Separator />
+              </div>
+              <CardFooter className="flex flex-wrap justify-end p-4">
+                <div className="space-x-2">
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    onClick={() => navigate(`/view-recipe/${recipe._id}`)}
+                  >
+                    <EyeIcon className="size-4 text-primary" />
+                  </Button>
+
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    onClick={() => navigate(`/update-recipe/${recipe._id}`)}
+                  >
+                    <Pencil className="size-4 text-primary" />
+                  </Button>
+
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <Button variant="ghost" size="icon">
+                        <Trash className="size-4 text-primary" />
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Are you absolutely sure?</DialogTitle>
+                        <DialogDescription>
+                          This action cannot be undone. This will permanently
+                          delete your recipe and remove the recipe from the
+                          database.
+                        </DialogDescription>
+                      </DialogHeader>
+                      <DialogFooter>
+                        <Button
+                          onClick={() => {
+                            toast.promise(
+                              axiosPublic.delete(`/recipes/${recipe._id}`),
+                              {
+                                loading: "Deleting recipe, Please wait ...",
+                                success: () => {
+                                  refetch();
+                                  return "Recipe deleted successfully";
+                                },
+                                error: (err) => {
+                                  toast.error("Failed to delete recipe");
+                                  return err.message;
+                                },
+                              }
+                            );
+                          }}
+                        >
+                          Delete
+                        </Button>
+                      </DialogFooter>
+                    </DialogContent>
+                  </Dialog>
+                </div>
+              </CardFooter>
+            </MotionCard>
+          ))}
       </div>
+
+      <Pagination>
+        <PaginationContent>
+          {[...Array(totalPages).keys()].map((page) => (
+            <PaginationItem
+              className="cursor-pointer"
+              key={page}
+              onClick={() => setCurrentPage(page)}
+            >
+              <PaginationLink isActive={currentPage === page}>
+                {page + 1}
+              </PaginationLink>
+            </PaginationItem>
+          ))}
+        </PaginationContent>
+      </Pagination>
     </>
   );
 };
